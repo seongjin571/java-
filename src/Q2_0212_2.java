426b1b032cfeb0d1a7095e1f07db4c26c74e3e4b
seongjin571 <gkstjdwls11@naver.com>
2019-02-14 20:44:52 +0900

    Q2 reform

---
+++ src/Q2_0212_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q2_0212_2{
+        public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        String number_input = number.nextLine();
+        String new_string="";
+
+        for (int i = 0; i < number_input.length(); i++){
+            int count=1;
+            for(int j=i; j<number_input.length()-1; j++) {
+                if (number_input.charAt(j) == number_input.charAt(j + 1))
+                    count++;
+                else
+                    break;
+            }
+            new_string+=number_input.charAt(i);
+            new_string+=count;
+            i+=count-1;
+        }
+
+        System.out.println(new_string);
+    }
+}
