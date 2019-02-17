3b8504398198281a394dd6d971b97a0f3598fe01
seongjin571 <gkstjdwls11@naver.com>
2019-02-17 19:24:09 +0900

    hw4

---
+++ src/Hw4.java
@@ -0,0 +1,27 @@
+class StringStack implements Stack {
+    Object stack="";
+   public int length() {
+        return stack.toString().length();
+    }
+
+    public Object pop() {
+       return stack;
+    }
+
+    public boolean push(Object ob) {
+       if(ob.equals(""))
+           return false;
+       else
+           stack=ob;
+           return true;
+
+    }
+}
+
+public class Hw4{
+    public static void main(String []args){
+        StringStack s=new StringStack();
+        s.push("안녕하세요");
+        System.out.println(s.pop());
+    }
+}
---
+++ src/Stack.java
@@ -0,0 +1,7 @@
+interface Stack {
+    int length();
+
+    Object pop();
+
+    boolean push(Object ob);
+}
