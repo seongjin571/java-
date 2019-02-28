21f19655a1fb0ed3dc39c52c3e94abf68a1c558a
seongjin571 <gkstjdwls11@naver.com>
2019-02-28 19:14:54 +0900

    study 4weeks

--- src/Q5_0226.java
+++ src/Q5_0226.java
@@ -1,44 +1,46 @@
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.ArrayList;
-////interface Comparable<T>{
-//////    void compareTo();
-////}
-//class Student implements Comparable<Student>{
-//    String name;
-//    int kor,eng,math;
-//    int total;
-//    Student(String name, int kor, int eng, int math){
-//        this.name=name;
-//        this.kor=kor;
-//        this.eng=eng;
-//        this.math=math;
-//        this.total=kor+eng+math;
-//    }
-//
-//    @Override
-////    public int compareTo(Student student){
-//////        return total.compareTo(student.total);
-////
-////    }
-////    public void setTotal(){
-////
-////    }
-//}
-//public class Q5_0226 {
-//    public static void main(String[] args) {
-//        List<Student> list = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            int korR=(int)(Math.random()*100);
-//            int engR=(int)(Math.random()*100);
-//            int mathR=(int)(Math.random()*100);
-//            Student student = new Student("한성진",korR,engR,mathR);
-//            list.add(student);
-//        }
-//        Collections.sort(list);
-//
-//
-//    }
-//}
-//
-//
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+class Student implements Comparable<Student>{
+    String name;
+    int kor,eng,math;
+    int total;
+    Student(String name, int kor, int eng, int math){
+        this.name=name;
+        this.kor=kor;
+        this.eng=eng;
+        this.math=math;
+        this.total=kor+eng+math;
+    }
+
+
+    @Override
+    public int compareTo(Student student){
+        if (total>student.total)
+            return -1;
+        else if(total<student.total)
+            return 1;
+        else
+            return 0;
+    }
+
+}
+public class Q5_0226 {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int korR=(int)(Math.random()*100);
+            int engR=(int)(Math.random()*100);
+            int mathR=(int)(Math.random()*100);
+            Student student = new Student("한성진",korR,engR,mathR);
+            list.add(student);
+        }
+        Collections.sort(list);
+        for(Student s: list){
+            System.out.println("이름: "+s.name+" 국어: "+s.kor+" 수학: "+s.math+" 영어: "+s.eng+" 총점 :"+s.total);
+        }
+    }
+}
+
+
--- src/Q6_0226.java
+++ src/Q6_0226.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
 public class Q6_0226 {
     public static void main(String []args){
-        ArrayList list1= new ArrayList();
-        ArrayList list2= new ArrayList();
-
+        ArrayList<Integer> list1= new ArrayList<>();
+        ArrayList<Integer> list2= new ArrayList<>();
+        ArrayList<Integer> list3= new ArrayList<>();
+        ArrayList<Integer> list4= new ArrayList<>();
+        Set <Integer> set=new HashSet<>();
 
         list1.add(1);
         list1.add(2);
@@ -13,27 +18,45 @@ public class Q6_0226 {
         list2.add(4);
         list2.add(5);
         list2.add(6);
-
-        System.out.println("교집합");
-        list3.retainAll(list4);
-        for(int i=0; i<list3.size(); i++)
-        {
-            System.out.print(list3.get(i)+" ");
+        for(int i=0; i<list1.size(); i++){
+            list3.add(list1.get(i));
+            list4.add(list2.get(i));
         }
-        System.out.println(" ");
-        list3=list1;
-        list4=list2;
-        System.out.print("합집합-");
-        list3.addAll(list4);
-        for(int i=0; i<list3.size(); i++)
-        {
-            System.out.print(list3.get(i)+" ");
+//        list3.add(list1);
+//        list4.add(list2);
+
+
+//        System.out.println("교집합");
+//        list1.retainAll(list2);
+//        for(Integer i: list1)
+//            System.out.print(i+" ");
+//
+//
+//        System.out.println("차집합-");
+//        list1.addAll(list2);
+//        list3.retainAll(list4);
+//        list1.removeAll(list3);
+//        for(Integer i: list1)
+//            System.out.print(i+" ");
+
+        System.out.println("합집합");
+        list1.addAll(list2);
+        //1번
+//        for(Integer i : list1){
+//            set.add(list1.get(i));
+//        }
+        //2번
+//        for(int i=0; i<list1.size(); i++){
+//            set.add(list1.get(i));
+//        }
+        //출력
+        for(Integer i :set){
+            System.out.print(i+" ");
         }
-        list3=list1;
-        list4=list2;
-        System.out.println("차집합");
-
-
+//        Iterator<Integer> iterator = set.iterator();
+//        while(iterator.hasNext()){
+//            System.out.print(iterator.next()+" ");
+//        }
 
 
     }
