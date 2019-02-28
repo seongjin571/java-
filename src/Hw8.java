09d85ca54ec20ca7afefe4916d92e4bfc3d4576c
seongjin571 <gkstjdwls11@naver.com>
2019-02-28 19:54:27 +0900

    Hw8/9

--- src/Hw8.java
+++ src/Hw8.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 import java.util.Vector;
 public class Hw8 {
     public static void main(String []args) {
-        Vector v = new Vector(10);
+        Vector<Integer> v = new Vector<>(10);
+        Scanner sacn = new Scanner(System.in);
+        int max_num=0;
+
         for (int i = 0; i < 10; i++) {
-
-
+            int num = sacn.nextInt();
+            v.add(num);
         }
+        for(int i : v){
+            max_num= (max_num<i) ? i:max_num;
+        }
+        System.out.println(max_num);
     }
 }
