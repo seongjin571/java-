acf22bfd0ceae01a2ed293cb770062f4b3ebd94e
seongjin571 <gkstjdwls11@naver.com>
2019-03-13 00:12:15 +0900

    Hw14

---
+++ src/Hw13.java
@@ -0,0 +1,28 @@
+public class Hw13 {
+    static boolean stopped = false;
+    public static void main(String []args){
+        Thread5 th1 = new Thread5();
+        th1.start();
+
+        try{
+            Thread.sleep(6*1000);
+        }catch (Exception e){
+
+        }
+        stopped = true;
+//        th1.interrupt();
+        System.out.println("stopped");
+    }
+}
+class Thread5 extends Thread{
+    public void run(){
+        for(int i = 0; !Hw13.stopped; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(3 * 1000);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
---
+++ src/Hw14.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+class WordGenerator extends Thread{
+    Vector<String> word = new Vector<>(9);
+    String [] data = {"태연", "유리", "윤아", "효연", "수영", "서현", "티파니", "써니", "제시카"};
+    public void run(){
+        for(int i = 0; i < 9; i++){
+            word.add(data[i]);
+//            System.out.println(data[i]);
+            try{
+                Thread.sleep(2000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class Hw14 {
+    public static void main(String[]args){
+        WordGenerator wg = new WordGenerator();
+        Scanner scan = new Scanner(System.in);
+        wg.start();
+        while (true){
+            System.out.println(wg.word;
+            System.out.println("]");
+            String name = scan.next();
+            wg.word.remove(name);
+        }
+    }
+}
